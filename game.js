30b27a4a4dcd86acf0b90678b15b771892762b1a
GitDaniR <dani.rachev@gmail.com>
2020-02-02 11:48:11 +0200

    ﻿initial commit

---
+++ game.js
@@ -0,0 +1,13 @@
+// Creating variables
+function update() {
+}
+function draw() {
+};
+function keyup(key) {
+    // Show the pressed keycode in the console
+    console.log("Pressed", key);
+};
+function mouseup() {
+    // Show coordinates of mouse on click
+    console.log("Mouse clicked at", mouseX, mouseY);
+};
